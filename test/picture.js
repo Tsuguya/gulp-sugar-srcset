05cd8c0803ad183db315a744cbe1d884aba62be3
Tsuguya Toma <tsuguya.toma@tgy.io>
2016-11-02 19:21:06 +0900

    update test

--- test/picture.js
+++ test/picture.js
@@ -16,11 +16,45 @@ const case2 = options({
 const case3 = options({
   picture: { extend: false }
 });
+const case4 = options({
+  picture: {
+    media: {
+      large: '(min-width: 1000px)',
+      medium: '(min-width: 800px)'
+    },
+    extend: false
+  }
+});
+const case5 = options({
+  responsiveSrcReplace: 320
+});
+const case6 = options({
+  responsive: true
+});
+const case7 = options({
+  responsive: true,
+  responsiveWidth: [320, 640, 900]
+});
+const case8 = options({
+  responsive: true,
+  responsiveWidth: [320, 640, 900],
+  picture: {
+    media: {
+      large: '(min-width: 1000px)',
+      medium: '(min-width: 800px)'
+    }
+  }
+});
 
 const txt = {
   case1: `[case1]`,
   case2: `[case2 - picture.media: { large: '(min-width: 1000px)', medium: '(min-width: 800px)' } -]`,
-  case3: `[case3 - picture.extend: false -]`
+  case3: `[case3 - picture.extend: false -]`,
+  case4: `[case4 - case 2 + 3 -]`,
+  case5: `[case5 - responsiveSrcReplace: 320 -]`,
+  case6: `[case6 - responsive: true -]`,
+  case7: `[case7 - responsive: true, responsiveWidth: [320, 640, 900] -]`,
+  case7: `[case7 - case 2 + 7 -]`
 };
 
 test(`${pfx} Resolution check. use ${txt.case1} `, t => {
@@ -35,9 +69,8 @@ test(`${pfx} Resolution check. use ${txt.case1} `, t => {
 <picture>
   <source media="large" srcset="path/to/filename-large.png,path/to/filename-large@2x.png 2x,path/to/filename-large@3x.png 3x">
   <source media="medium" srcset="path/to/filename-medium.png,path/to/filename-medium@2x.png 2x,path/to/filename-medium@3x.png 3x">
-  <img src="path/to/filename-small.png" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
-</picture>
-`;
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
 
   t.equal(main(html, case1), correct);
   t.end();
@@ -55,9 +88,8 @@ test(`${pfx} Resolution check. use ${txt.case2} `, t => {
 <picture>
   <source media="(min-width: 1000px)" srcset="path/to/filename-large.png,path/to/filename-large@2x.png 2x,path/to/filename-large@3x.png 3x">
   <source media="(min-width: 800px)" srcset="path/to/filename-medium.png,path/to/filename-medium@2x.png 2x,path/to/filename-medium@3x.png 3x">
-  <img src="path/to/filename-small.png" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
-</picture>
-`;
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
 
   t.equal(main(html, case2), correct);
   t.end();
@@ -73,13 +105,31 @@ test(`${pfx} Resolution check. use ${txt.case3} `, t => {
 
   const correct = `
 <picture>
-  <source src="path/to/filename-large.png" media="large" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
-  <source src="path/to/filename-medium.png" media="medium" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
-  <img src="path/to/filename-small.png" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
-</picture>
-`;
+  <source media="large" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
+  <source media="medium" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
 
   t.equal(main(html, case3), correct);
+  t.end();
+});
+
+test(`${pfx} Resolution check. use ${txt.case4} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large.png" media="large" srcset="1x, 2x, 3x">
+  <source src="path/to/filename-medium.png" media="medium" srcset="1x, 2x, 3x">
+  <img src="path/to/filename-small.png" srcset="1x, 2x, 3x" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="(min-width: 1000px)" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
+  <source media="(min-width: 800px)" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
+
+  t.equal(main(html, case4), correct);
   t.end();
 });
 
@@ -96,9 +146,8 @@ test(`${pfx} Resolution check. No src. use ${txt.case1} `, t => {
 <picture>
   <source media="large" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
   <source media="medium" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
-  <img src="path/to/filename-small.png" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
-</picture>
-`;
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
 
   t.equal(main(html, case1), correct);
   t.end();
@@ -116,9 +165,8 @@ test(`${pfx} Resolution check. No src. use ${txt.case2} `, t => {
 <picture>
   <source media="(min-width: 1000px)" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
   <source media="(min-width: 800px)" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
-  <img src="path/to/filename-small.png" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
-</picture>
-`;
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
 
   t.equal(main(html, case2), correct);
   t.end();
@@ -136,10 +184,219 @@ test(`${pfx} Resolution check. No src. use ${txt.case3} `, t => {
 <picture>
   <source media="large" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
   <source media="medium" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
-  <img src="path/to/filename-small.png" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
-</picture>
-`;
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
 
   t.equal(main(html, case3), correct);
+  t.end();
+});
+
+test(`${pfx} Resolution check. No src. use ${txt.case4} `, t => {
+  const html = `
+<picture>
+  <source media="large" srcset="1x, 2x, 3x">
+  <source media="medium" srcset="1x, 2x, 3x">
+  <img src="path/to/filename-small.png" srcset="1x, 2x, 3x" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="(min-width: 1000px)" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
+  <source media="(min-width: 800px)" srcset="path/to/filename-small.png,path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x" alt="">
+</picture>`;
+
+  t.equal(main(html, case4), correct);
+  t.end();
+});
+
+test(`${pfx} Resolution check. extend. use ${txt.case1} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large@3x.png" media="large">
+  <source src="path/to/filename-medium@3x.png" media="medium">
+  <img src="path/to/filename-small@3x.png" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="large" srcset="path/to/filename-large.png,path/to/filename-large@2x.png 2x,path/to/filename-large@3x.png 3x">
+  <source media="medium" srcset="path/to/filename-medium.png,path/to/filename-medium@2x.png 2x,path/to/filename-medium@3x.png 3x">
+  <img src="path/to/filename-small.png" alt="" srcset="path/to/filename-small@2x.png 2x,path/to/filename-small@3x.png 3x">
+</picture>`;
+
+  t.equal(main(html, case1), correct);
+  t.end();
+});
+
+
+test(`${pfx} Responsive check. use ${txt.case1} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large.png" media="large" srcset="320w, 640w, 900w">
+  <source src="path/to/filename-medium.png" media="medium" srcset="320w, 640w, 900w">
+  <img src="path/to/filename-small.png" srcset="320w, 640w, 900w" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="large" srcset="path/to/filename-large-320w.png 320w,path/to/filename-large-640w.png 640w,path/to/filename-large-900w.png 900w">
+  <source media="medium" srcset="path/to/filename-medium-320w.png 320w,path/to/filename-medium-640w.png 640w,path/to/filename-medium-900w.png 900w">
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w" alt="">
+</picture>`;
+
+  t.equal(main(html, case1), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. use ${txt.case2} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large.png" media="large" srcset="320w, 640w, 900w">
+  <source src="path/to/filename-medium.png" media="medium" srcset="320w, 640w, 900w">
+  <img src="path/to/filename-small.png" srcset="320w, 640w, 900w" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="(min-width: 1000px)" srcset="path/to/filename-large-320w.png 320w,path/to/filename-large-640w.png 640w,path/to/filename-large-900w.png 900w">
+  <source media="(min-width: 800px)" srcset="path/to/filename-medium-320w.png 320w,path/to/filename-medium-640w.png 640w,path/to/filename-medium-900w.png 900w">
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w" alt="">
+</picture>`;
+
+  t.equal(main(html, case2), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. use ${txt.case5} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large.png" media="large" srcset="320w, 640w, 900w">
+  <source src="path/to/filename-medium.png" media="medium" srcset="320w, 640w, 900w">
+  <img src="path/to/filename-small.png" srcset="320w, 640w, 900w" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="large" srcset="path/to/filename-large-320w.png 320w,path/to/filename-large-640w.png 640w,path/to/filename-large-900w.png 900w">
+  <source media="medium" srcset="path/to/filename-medium-320w.png 320w,path/to/filename-medium-640w.png 640w,path/to/filename-medium-900w.png 900w">
+  <img src="path/to/filename-small-320w.png" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w" alt="">
+</picture>`;
+
+  t.equal(main(html, case5), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. No src. use ${txt.case1} `, t => {
+  const html = `
+<picture>
+  <source media="large" srcset="320w, 640w, 900w">
+  <source media="medium" srcset="320w, 640w, 900w">
+  <img src="path/to/filename-small.png" srcset="320w, 640w, 900w" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="large" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w">
+  <source media="medium" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w">
+  <img src="path/to/filename-small.png" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w" alt="">
+</picture>`;
+
+  t.equal(main(html, case1), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. No src. use ${txt.case5} `, t => {
+  const html = `
+<picture>
+  <source media="large" srcset="320w, 640w, 900w">
+  <source media="medium" srcset="320w, 640w, 900w">
+  <img src="path/to/filename-small.png" srcset="320w, 640w, 900w" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="large" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w">
+  <source media="medium" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w">
+  <img src="path/to/filename-small-320w.png" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w,path/to/filename-small-900w.png 900w" alt="">
+</picture>`;
+
+  t.equal(main(html, case5), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. extend. use ${txt.case1} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large-640w.png" media="large">
+  <source src="path/to/filename-medium-640w.png" media="medium">
+  <img src="path/to/filename-small-640w.png" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source src="path/to/filename-large-640w.png" media="large">
+  <source src="path/to/filename-medium-640w.png" media="medium">
+  <img src="path/to/filename-small-640w.png" alt="">
+</picture>`;
+
+  t.equal(main(html, case1), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. extend. use ${txt.case6} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large-640w.png" media="large">
+  <source src="path/to/filename-medium-640w.png" media="medium">
+  <img src="path/to/filename-small-640w.png" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="large">
+  <source media="medium">
+  <img src="path/to/filename-small-640w.png" alt="">
+</picture>`;
+
+  t.equal(main(html, case6), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. extend. use ${txt.case7} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large-640w.png" media="large">
+  <source src="path/to/filename-medium-640w.png" media="medium">
+  <img src="path/to/filename-small-640w.png" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="large" srcset="path/to/filename-large-320w.png 320w,path/to/filename-large-640w.png 640w">
+  <source media="medium" srcset="path/to/filename-medium-320w.png 320w,path/to/filename-medium-640w.png 640w">
+  <img src="path/to/filename-small-640w.png" alt="" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w">
+</picture>`;
+
+  t.equal(main(html, case7), correct);
+  t.end();
+});
+
+test(`${pfx} Responsive check. extend. use ${txt.case8} `, t => {
+  const html = `
+<picture>
+  <source src="path/to/filename-large-640w.png" media="large">
+  <source src="path/to/filename-medium-640w.png" media="medium">
+  <img src="path/to/filename-small-640w.png" alt="">
+</picture>`;
+
+  const correct = `
+<picture>
+  <source media="(min-width: 1000px)" srcset="path/to/filename-large-320w.png 320w,path/to/filename-large-640w.png 640w">
+  <source media="(min-width: 800px)" srcset="path/to/filename-medium-320w.png 320w,path/to/filename-medium-640w.png 640w">
+  <img src="path/to/filename-small-640w.png" alt="" srcset="path/to/filename-small-320w.png 320w,path/to/filename-small-640w.png 640w">
+</picture>`;
+
+  t.equal(main(html, case8), correct);
   t.end();
 });
